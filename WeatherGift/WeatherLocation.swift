bd68b8475cbca329b9d340c5b4b74bd8cadb8449
Hannah Jiang <jiangbz@bc.edu>
2020-03-09 00:16:19 -0400

    7.2 WeatherLocations List Table View

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Hannah Jiang on 3/8/20.
+//  Copyright © 2020 Hannah Jiang. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation{
+    var name: String
+    var latitude: Double
+    var longitude: Double
+    
+}
